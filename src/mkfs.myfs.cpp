fa2efc19fddcd0c680ecf61d1ffc45cbc05d9007
Lukas Hügle <lhuegle@icloud.com>
2018-12-11 16:57:54 +0100

    Test für Git Integration Telegram: ->Main File von mkfs.myfs geändert

--- src/mkfs.myfs.cpp
+++ src/mkfs.myfs.cpp
@@ -5,14 +5,19 @@
 //  Created by Oliver Waldhorst on 07.09.17.
 //  Copyright © 2017 Oliver Waldhorst. All rights reserved.
 //
-
+#include <fstream>
 #include "myfs.h"
 #include "blockdevice.h"
 #include "macros.h"
 
 int main(int argc, char *argv[]) {
 
+    BlockDevice test;
+    test.create(argv[0]);
+
+
     // TODO: Implement file system generation & copying of files here
-    
+
     return 0;
 }
+
