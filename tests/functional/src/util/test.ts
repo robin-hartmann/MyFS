0da5b9735971315ac99a35d7ac07cf48f7db396d
Robin Hartmann <contact.robin.hartmann@gmail.com>
2019-01-06 16:01:32 +0100

    [test-func] Remove unused code

--- tests/functional/src/util/test.ts
+++ tests/functional/src/util/test.ts
@@ -1,7 +1,3 @@
-// import ava, { Macro, TestInterface } from 'ava';
-// import { readFileSync } from 'fs';
-// import { resolve } from 'path';
-
 import ava, { ExecutionContext as UntypedExecutionContext, TestInterface } from 'ava';
 import { SynchrounousResult } from 'tmp';
 
@@ -14,18 +10,3 @@ export interface Context {
 export type ExecutionContext = UntypedExecutionContext<Context>;
 
 export const test = ava as TestInterface<Context>;
-
-// const fileMacro: Macro<[string], Context> = (t, fileName) => {
-//   const buffer = readFileSync(resolve(t.context.mountDir.name, fileName));
-//   // in proper tests the buffer should not be cropped,
-//   // but instead the entire buffer should be examined,
-//   // e.g. when the contents of two files are compared for equality
-//   // at most the buffer should be cropped to the files' sizes
-//   const content = buffer.toString(undefined, 0, buffer.indexOf('\0'));
-//   t.is(content, getExpectedFileContent(fileName));
-// };
-
-// fileMacro.title = (fileName = '') =>
-//   `contains file '${fileName}' with content '${getExpectedFileContent(fileName)}'`;
-
-// export { fileMacro };
