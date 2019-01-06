92603a37d9aabebc28094f208262f5cc00bc4814
Robin Hartmann <contact.robin.hartmann@gmail.com>
2019-01-06 15:29:14 +0100

    [test-func] Fix promise not being awaited

--- tests/functional/src/spec/mount.spec.ts
+++ tests/functional/src/spec/mount.spec.ts
@@ -21,7 +21,7 @@ test.serial.before('mounts properly', async (t) => {
 test.after.always('unmounts properly', async (t) => {
   t.log(t.context);
   await unmount(t);
-  t.false(isMounted(t));
+  t.false(await isMounted(t));
 
   unlinkSync(t.context.containerFile);
   t.context.logFile.removeCallback();
