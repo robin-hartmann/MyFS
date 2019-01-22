da9003cf66c1d93b6dc096d424711648b4781d2c
Robin Hartmann <contact.robin.hartmann@gmail.com>
2019-01-22 14:26:40 +0100

    [test-func] Add function to generate test data

--- tests/functional/src/config.ts
+++ tests/functional/src/config.ts
@@ -2,6 +2,7 @@ import { resolve } from 'path';
 import { sync as getPkgRoot } from 'pkg-dir';
 
 const PKG_ROOT = getPkgRoot(__dirname) as string;
+const PKG_RES = resolve(PKG_ROOT, 'res');
 
 const FS_ROOT = resolve(PKG_ROOT, '../..');
 const FS_BIN = resolve(FS_ROOT, 'cmake-build-debug/bin');
@@ -10,5 +11,8 @@ export default Object.freeze({
   BINARIES: {
     MKFS: resolve(FS_BIN, 'mkfs.myfs'),
     MOUNT: resolve(FS_BIN, 'mount.myfs'),
+  },
+  RESOURCES: {
+    LOREM_IPSUM: resolve(PKG_RES, 'lorem-ipsum-1024.txt'),
   },
 });
---
+++ tests/functional/src/util/data.ts
@@ -0,0 +1,24 @@
+import { readFileSync } from 'fs';
+import config from '../config';
+
+const LOREM_IPSUM = readFileSync(config.RESOURCES.LOREM_IPSUM);
+
+export const generateData = (byteCount: number) => {
+  const data = Buffer.allocUnsafe(byteCount);
+  let remainingBytes = byteCount;
+
+  while (remainingBytes > 0) {
+    remainingBytes -= LOREM_IPSUM.copy(
+      data,
+      byteCount - remainingBytes,
+      0,
+      Math.min(LOREM_IPSUM.byteLength, remainingBytes),
+    );
+  }
+
+  if (remainingBytes) {
+    throw 'remainingBytes must be 0';
+  }
+
+  return data;
+};
