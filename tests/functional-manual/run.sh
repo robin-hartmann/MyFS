5071203121982fc8e96adf4e5a9ed8559316e23c
Robin Hartmann <contact.robin.hartmann@gmail.com>
2019-02-24 18:38:06 +0100

    [test-func] Make all paths relative to scriptDir

--- tests/functional-manual/run.sh
+++ tests/functional-manual/run.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 scriptDir=$(dirname "$0")
 
-binDir="$scriptDir/../../cmake-build-debug/bin/"
+binDir="$scriptDir/../../cmake-build-debug/bin"
 mkfs="$binDir/mkfs.myfs"
 mount="$binDir/mount.myfs"
 
 helloWorld="$scriptDir/hello-world.txt"
 
 printf "\n*** cleanup ***\n"
-rm -rf tmp/
-mkdir tmp/ tmp/mnt/
+rm -rf $scriptDir/tmp/
+mkdir $scriptDir/tmp/ $scriptDir/tmp/mnt/
 
 printf "\n*** mkfs ***\n"
-$mkfs tmp/container $helloWorld
+$mkfs $scriptDir/tmp/container $helloWorld
 
 printf "\n*** mount ***\n"
-$mount tmp/container tmp/log tmp/mnt/
+$mount $scriptDir/tmp/container $scriptDir/tmp/log $scriptDir/tmp/mnt/
 
 printf "\n*** read pre-initialized file ***\n"
-cat tmp/mnt/hello-world.txt
+cat $scriptDir/tmp/mnt/hello-world.txt
 
 printf "\n*** create file manually ***\n"
-printf Hello World! > tmp/mnt/hello-world-manual.txt
+printf Hello World! > $scriptDir/tmp/mnt/hello-world-manual.txt
 
 printf "\n*** read manually created file ***\n"
-cat tmp/mnt/hello-world-manual.txt
+cat $scriptDir/tmp/mnt/hello-world-manual.txt
 
 printf "\n*** unmount ***\n"
-sudo umount tmp/mnt/
+sudo umount $scriptDir/tmp/mnt/
