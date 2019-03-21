6e885934c0ef4d260481dcf208391cf325810e9b
Robin Hartmann <contact.robin.hartmann@gmail.com>
2019-03-21 17:22:26 +0100

    [test-functional-manual] Simplify commands

--- tests/functional-manual/run.sh
+++ tests/functional-manual/run.sh
@@ -15,60 +15,57 @@ printf "\n*** mkfs ***\n"
 $mkfs $scriptDir/tmp/container $helloWorld
 
 printf "\n*** mount ***\n"
-sudo $mount $scriptDir/tmp/container $scriptDir/tmp/log.txt $scriptDir/tmp/mnt/ -s -o allow_other
-# alternatively: run without -o allow_other:
-# sudo $mount $scriptDir/tmp/container $scriptDir/tmp/log.txt $scriptDir/tmp/mnt/ -s
-# but then all read access to files needs to be done with sudo, as well
+$mount $scriptDir/tmp/container $scriptDir/tmp/log.txt $scriptDir/tmp/mnt/ -s
 
 printf "\n*** read pre-initialized file ***\n"
 cat $scriptDir/tmp/mnt/hello-world.txt
 
 printf "\n*** create file manually ***\n"
-sudo bash -c "echo 'Hello Manual World!' > $scriptDir/tmp/mnt/hello-manual-world.txt"
+echo "Hello Manual World!" > $scriptDir/tmp/mnt/hello-manual-world.txt
 
 printf "\n*** read manually created file ***\n"
 cat $scriptDir/tmp/mnt/hello-manual-world.txt
 
 
 printf "\n*** initial append ***\n"
-sudo bash -c "echo 'initial append' >> '$scriptDir/tmp/mnt/append.txt'"
+echo "initial append" >> "$scriptDir/tmp/mnt/append.txt"
 cat "$scriptDir/tmp/mnt/append.txt"
 
 printf "\n*** 2nd append ***\n"
-sudo bash -c "echo '2nd append' >> '$scriptDir/tmp/mnt/append.txt'"
+echo "2nd append" >> "$scriptDir/tmp/mnt/append.txt"
 cat "$scriptDir/tmp/mnt/append.txt"
 
 printf "\n*** 3rd append ***\n"
-sudo bash -c "echo '3rd append' >> '$scriptDir/tmp/mnt/append.txt'"
+echo "3rd append" >> "$scriptDir/tmp/mnt/append.txt"
 cat "$scriptDir/tmp/mnt/append.txt"
 
 printf "\n*** 4th append ***\n"
-sudo bash -c "echo '4th append' >> '$scriptDir/tmp/mnt/append.txt'"
+echo "4th append" >> "$scriptDir/tmp/mnt/append.txt"
 cat "$scriptDir/tmp/mnt/append.txt"
 
 printf "\n*** initial overwrite append: same length ***\n"
-sudo bash -c "echo 'same length....................................' > '$scriptDir/tmp/mnt/append.txt'"
+echo "same length...................................." > "$scriptDir/tmp/mnt/append.txt"
 cat "$scriptDir/tmp/mnt/append.txt"
 
 printf "\n*** 2nd overwrite append: longer length ***\n"
-sudo bash -c "echo 'longer length...................................' > '$scriptDir/tmp/mnt/append.txt'"
+echo "longer length..................................." > "$scriptDir/tmp/mnt/append.txt"
 cat "$scriptDir/tmp/mnt/append.txt"
 
 
 printf "\n*** initial overwrite ***\n"
-sudo bash -c "echo 'initial overwrite' > '$scriptDir/tmp/mnt/overwrite.txt'"
+echo "initial overwrite" > "$scriptDir/tmp/mnt/overwrite.txt"
 cat "$scriptDir/tmp/mnt/overwrite.txt"
 
 printf "\n*** 2nd overwrite: same length ***\n"
-sudo bash -c "echo 'same length......' > '$scriptDir/tmp/mnt/overwrite.txt'"
+echo "same length......" > "$scriptDir/tmp/mnt/overwrite.txt"
 cat "$scriptDir/tmp/mnt/overwrite.txt"
 
 printf "\n*** 3rd overwrite: longer length ***\n"
-sudo bash -c "echo 'longer length.....' > '$scriptDir/tmp/mnt/overwrite.txt'"
+echo "longer length....." > "$scriptDir/tmp/mnt/overwrite.txt"
 cat "$scriptDir/tmp/mnt/overwrite.txt"
 
 printf "\n*** 4th overwrite: shorter length ***\n"
-sudo bash -c "echo 'shorter length' > '$scriptDir/tmp/mnt/overwrite.txt'"
+echo "shorter length" > "$scriptDir/tmp/mnt/overwrite.txt"
 cat "$scriptDir/tmp/mnt/overwrite.txt"
 
 
