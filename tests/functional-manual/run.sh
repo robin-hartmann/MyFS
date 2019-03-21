a4bd94d27a0a772b742a9271666596c0632cb70d
Robin Hartmann <contact.robin.hartmann@gmail.com>
2019-03-21 14:36:29 +0100

    [test-functional-manual] Add append and overwrite tests

--- tests/functional-manual/run.sh
+++ tests/functional-manual/run.sh
@@ -29,5 +29,48 @@ sudo bash -c "echo 'Hello Manual World!' > $scriptDir/tmp/mnt/hello-manual-world
 printf "\n*** read manually created file ***\n"
 cat $scriptDir/tmp/mnt/hello-manual-world.txt
 
+
+printf "\n*** initial append ***\n"
+sudo bash -c "echo 'initial append' >> '$scriptDir/tmp/mnt/append.txt'"
+cat "$scriptDir/tmp/mnt/append.txt"
+
+printf "\n*** 2nd append ***\n"
+sudo bash -c "echo '2nd append' >> '$scriptDir/tmp/mnt/append.txt'"
+cat "$scriptDir/tmp/mnt/append.txt"
+
+printf "\n*** 3rd append ***\n"
+sudo bash -c "echo '3rd append' >> '$scriptDir/tmp/mnt/append.txt'"
+cat "$scriptDir/tmp/mnt/append.txt"
+
+printf "\n*** 4th append ***\n"
+sudo bash -c "echo '4th append' >> '$scriptDir/tmp/mnt/append.txt'"
+cat "$scriptDir/tmp/mnt/append.txt"
+
+printf "\n*** initial overwrite append: same length ***\n"
+sudo bash -c "echo 'same length....................................' > '$scriptDir/tmp/mnt/append.txt'"
+cat "$scriptDir/tmp/mnt/append.txt"
+
+printf "\n*** 2nd overwrite append: longer length ***\n"
+sudo bash -c "echo 'longer length...................................' > '$scriptDir/tmp/mnt/append.txt'"
+cat "$scriptDir/tmp/mnt/append.txt"
+
+
+printf "\n*** initial overwrite ***\n"
+sudo bash -c "echo 'initial overwrite' > '$scriptDir/tmp/mnt/overwrite.txt'"
+cat "$scriptDir/tmp/mnt/overwrite.txt"
+
+printf "\n*** 2nd overwrite: same length ***\n"
+sudo bash -c "echo 'same length......' > '$scriptDir/tmp/mnt/overwrite.txt'"
+cat "$scriptDir/tmp/mnt/overwrite.txt"
+
+printf "\n*** 3rd overwrite: longer length ***\n"
+sudo bash -c "echo 'longer length.....' > '$scriptDir/tmp/mnt/overwrite.txt'"
+cat "$scriptDir/tmp/mnt/overwrite.txt"
+
+printf "\n*** 4th overwrite: shorter length ***\n"
+sudo bash -c "echo 'shorter length' > '$scriptDir/tmp/mnt/overwrite.txt'"
+cat "$scriptDir/tmp/mnt/overwrite.txt"
+
+
 printf "\n*** unmount ***\n"
 sudo umount $scriptDir/tmp/mnt/
